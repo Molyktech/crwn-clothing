146cdd71dbbfde1428658d4bdcb2c53c112d04a2
molyktech <modupeadebayo001@gmail.com>
2022-11-21 23:37:51 +0100

    nested routes

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,33 @@
+import { createContext, useState, useEffect } from "react";
+
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments("categories");
+      setCategoriesMap(categoryMap);
+    };
+
+    getCategoriesMap();
+  }, []);
+
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
+// import SHOP_DATA from "../shop-data.js";
+// import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils";
+// how to add categories to Firebase run only once then delete
+// useEffect(() => {
+//     addCollectionAndDocuments("categories", SHOP_DATA);
+//   }, []);
